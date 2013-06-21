b81ca825e895d321b967aa90d16c25c5b4dec220
joyjding <joy.j.ding@gmail.com>
2013-06-21 16:56:40 -0700

    finished Ex05 hooray

--- lettercount.py
+++ lettercount.py
@@ -20,15 +20,16 @@ indata = in_file.read()
 
 #for loop to loop through the alphabet, with a counter?, print how many times it occurred
 
-# try # 2 with nested for loops:
-for letter in range(65,91):
-	count = 0
-	for i in range(len(indata)):
-		if	ord(indata[i]) == letter or ord(indata[i]) == letter+32:
-				count = count +1
-	print chr(letter), count
+# try # 2 with nested for loops. Unfortunately, this was too slow. But it works!!!
+# for letter in range(65,91):
+# 	count = 0
+# 	for i in range(len(indata)):
+# 		if	ord(indata[i]) == letter or ord(indata[i]) == letter+32:
+# 				count = count +1
+# 	print chr(letter), count
 
-# This try did not work. Coming back to this one. 
+# Try #3. This try did not work.
+
 # alphabet="A B C D E F G H I J K L M N O P Q R S T U V W X Y Z a b c d e f g h i j k l m n o p q r s t u v w x y z"
 
 # list_alpha = alphabet.split(" ")
@@ -40,5 +41,24 @@ for letter in range(65,91):
 # 			alpha_index = alpha_index + 1
 # 	print alpha_index
 
+# Next up, solving with a list!
+
+answer = [0]*26
+#answer [0:26:1] = 0
+#answer = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
+# for char in indata.lower():
+# 	print ord(char)
+# 	answer[ord(char)-97]="bunny"
+# 	print answer
+
+for char in indata.lower():
+	if ord(char) in range (97, 122):
+		answer[ord(char)-97] +=1
+
 in_file.close()
-print "we are done!!!!!!!!!!!!!"
+
+for i in answer:
+	print i
+
+
